8a33612020a90400a554772c72a035cc5ce76760
muddu2602 <abhishek.muddu26@gmail.com>
2022-08-24 14:57:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello OFSS");
 		System.out.println("Hello INDIA");
 		System.out.println("All are one");
+		System.out.println("New Change is detected");
 	}
 }
