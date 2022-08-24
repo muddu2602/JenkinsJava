1d8d10e968697abc2294d14b24700e3f9dc5351b
muddu2602 <abhishek.muddu26@gmail.com>
2022-08-24 14:47:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello Abhishek");
+		System.out.println("Hello OFSS");
+		System.out.println("Hello INDIA");
+		System.out.println("All are one");
 	}
 }
